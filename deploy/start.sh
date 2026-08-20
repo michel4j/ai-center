693d69e71140529732bc65006d14f0886527ef70
Michel Fodje <michel4j@gmail.com>
2026-08-20 11:04:53 -0600

    [REMOVE] Remove app.server script and update start.sh to use app.epics; modify video stream parameters

--- deploy/start.sh
+++ deploy/start.sh
@@ -3,17 +3,16 @@
 
 
 # -------------- Environment Parameters (MODIFY)
-app_cmd=/apps/ai-centering/bin/app.server  # location where application is installed
-epics_env=/apps/profile.d/epics.sh         # EPICS environment setup script
+app_cmd=/apps/ai-centering/bin/app.epics  # location where application is installed
+epics_env=/apps/profile.d/epics.sh        # EPICS environment setup script
 
 # -------------- Device Parameters (MODIFY)
-device="AIC1000-001"                                # Device Name
-appdir="/ioc/instances/${device}"                   # IOC Directory
-server="video-server.example.com"                   # Redis Video Server hostname or IP address
-camera="0030180F06E5"                               # Redis Video Stream ID
-model="/yolo/models/scrn-20250417-crystals-v8m"     # Path to YOLO model (ONNX or Darknet) files
+device="AIC1000-001"                                  # Device Name
+appdir="/ioc/instances/${device}"                     # IOC Directory
+video="https://video-server.example.com/video.mjpeg"  # Video Stream URI
+model="/yolo/models/model-v1.pt"       # Path to YOLO model (ONNX or PyTorch) files
 
 # Prepare environment and  Source function library.
 . ${epics_env}
 cd $appdir
-exec $app_cmd --device $device --server $server --camera $camera --model $model
+exec $app_cmd --device $device --video $video --model $model
